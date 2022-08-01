e085097aca04cb3398f28cb085cb4fbed07b8e37
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-08-01 14:06:27 +0200

    router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./styles/glopal.scss"
-
+import { Route, Routes ,BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="Tours" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
